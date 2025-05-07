de37179a8cc3e0f434fc3cb366d9774ddcb6a516
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2025-05-06 18:28:07 -0600

    Initial pages

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def login (request):
+    return render(request, 'login.html')
+
+def policy (request):
+    return render(request, 'policy.html')
+
 # Create your views here.
