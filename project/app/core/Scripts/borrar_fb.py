44c2c0cd3eaa07184d2468c3f34628b747ffb076
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-02-18 13:07:40 -0600

    debug: used them for deleting certain data in db

--- project/app/core/Scripts/borrar_fb.py
+++ project/app/core/Scripts/borrar_fb.py
@@ -12,21 +12,18 @@ cred_path = os.path.join(BASE_DIR, 'riskmapper-jc-cc-firebase-adminsdk-fbsvc-a6e
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')  # Reemplaza por el nombre de tu proyecto Django
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')  
 # django.setup()
 
 # 2. Inicializa el cliente de Firestore
 db = firestore.client()
 
-# 3. Configura los parámetros del filtro
-coleccion = "Eventos"         
-# campo_filtro = "Estado_hechos"                
-# valor_a_borrar = "Colima"    
-# 
-inicio_dia = datetime(2025, 9, 9, 0, 0, 0, tzinfo=timezone.utc)
-fin_dia = datetime(2025, 9, 9, 23, 59, 59, 999999, tzinfo=timezone.utc)
+# 3. Configura los parámetros del filtro        
 
-docs = db.collection(coleccion) \
+inicio_dia = datetime(2026, 2, 14, 0, 0, 0, tzinfo=timezone.utc)
+fin_dia = datetime(2026, 2, 14, 23, 59, 59, 999999, tzinfo=timezone.utc)
+
+docs = db.collection('Eventos') \
          .where('updatedAt', '>=', inicio_dia) \
          .where('updatedAt', '<=', fin_dia) \
          .get()
@@ -40,15 +37,15 @@ for doc in docs:
 print("Documentos eliminados con éxito.")
 
 # 4. Obtener y borrar documentos que coincidan
-# docs = db.collection(coleccion).where(campo_filtro, "==", valor_a_borrar).stream()
+# docs = db.collection('Eventos').where(campo_filtro, "==", "").stream()
 
 # contador = 0
 # for doc in docs:
 #     print(f"Borrando documento ID: {doc.id}")
-#     db.collection(coleccion).document(doc.id).delete()
+#     db.collection('Eventos').document(doc.id).delete()
 #     contador += 1
 
 # cache.delete(CACHE_KEY_LAST_UPDATE)
 # cache.delete(CACHE_KEY_MARKERS)
 
-# print(f"✅ Total de documentos eliminados: {contador}")
+# print(f" Total de documentos eliminados: {contador}")
