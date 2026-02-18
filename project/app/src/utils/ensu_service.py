72006cc9e67fcc905d64867b5067681bb4f8ab73
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-02-18 13:09:37 -0600

    fix: fixed the search for insecurity perception of a place

--- project/app/src/utils/ensu_service.py
+++ project/app/src/utils/ensu_service.py
@@ -5,13 +5,14 @@ import requests
 def contextEnsu(municipio):
     url = 'https://www.inegi.org.mx/contenidos/saladeprensa/boletines/2026/ensu/ENSU2026_01_RR.pdf'
     response = requests.get(url)
-    reduced_context =''
+    extract = ""
 
     with pdfplumber.open(io.BytesIO(response.content)) as pdf:
-        for page in pdf.pages:
-            text = page.extract_text()
-            if municipio in text:
-                reduced_context += text
-                return f"Basado en este texto: {reduced_context}, ¿Cuál es el porcentaje de inseguridad en {municipio} en diciembre 2025?"
-            else:
-                return "Porcentaje de inseguridad no encontrado"
+
+        table = pdf.pages[6].extract_table()
+        row_municipio = [row for row in table if row and municipio in str(row)]
+        extract = str(row_municipio)
+        if extract:
+            return f"Basado en este texto: {extract}, ¿Cuál es el porcentaje de inseguridad en {municipio} en diciembre 2025? Unicamente toma en cuenta {municipio}, ningun otro mencionado en la tabla"
+        else:
+            return "Porcentaje de inseguridad no encontrado"
