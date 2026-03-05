2f3c7bef1814744f2426ff54682b1ae05634e463
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-03-04 20:38:59 -0600

    Prod: credentials path for deployment

--- project/app/core/auth/firebase_config.py
+++ project/app/core/auth/firebase_config.py
@@ -12,13 +12,10 @@ FIREBASE_API_KEY= config('FIREBASE_API_KEY', default=None)
 # DEBUGGING
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 cred_path = os.path.join(BASE_DIR, 'etc','secrets','riskmapper-jc-cc-firebase-adminsdk-fbsvc-a6ee255385.json')
-cred = credentials.Certificate(cred_path)
 
 #Production
-# firebase_json = json.loads(config('FIREBASE_JSON'))
-# # firebase_json = json.loads(firebase_json_str) 
-# firebase_json['private_key'] = firebase_json['private_key'].replace('\\n', '\n')
-# cred = credentials.Certificate(firebase_json)
+cred_path = '/etc/secrets/riskmapper-jc-cc-firebase-adminsdk-fbsvc-a6ee255385.json'
+cred = credentials.Certificate(cred_path)
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
