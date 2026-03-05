122a839f4905541fa6d32c2631ed0c5a24f6c679
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-03-04 18:52:38 -0600

    Fix: domain

--- project/project/settings.py
+++ project/project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','es-cerberus.com']
+ALLOWED_HOSTS = ['127.0.0.1','www.es-cerberus.com']
 
 
 # Application definition
