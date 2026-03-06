058d2a801abaac2ab01c732fa19aa7687e9da932
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-03-06 14:21:27 -0600

    prod: fix

--- project/app/core/tasks.py
+++ project/app/core/tasks.py
@@ -4,7 +4,7 @@ from app.src.report_generation import process_report
 from app.core.auth.firebase_config import db
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={'max_retries': 3})
-def generate_report_task(data,uid):
+def generate_report_task(self,data,uid):
     result = process_report(data,uid)
 
     report_id = str(uuid.uuid4())
