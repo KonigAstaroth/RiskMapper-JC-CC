0444d41204c64e1317e4ce447d41bc29fdec1a64
Guillermo Gonzalez <guillermogonzalezg2005@gmail.com>
2026-03-09 13:33:29 -0600

    prod: minor fix

--- project/app/src/report_generation_service.py
+++ project/app/src/report_generation_service.py
@@ -35,7 +35,7 @@ def check_report_status(request):
     task = AsyncResult(task_id)
 
     if task.state == "SUCCESS":
-        request.session.get('generating_report') = False
+        request.session['generating_report'] = False
 
         report_id = task.result
 
